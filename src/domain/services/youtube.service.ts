44761edce608ad49da540858a615722631829309
Gabriel H. Finotti <finotti.dev@gmail.com>
2025-07-26 22:04:01 -0300

    fix: correct AbortSignal type in YouTube service interface

--- src/domain/services/youtube.service.ts
+++ src/domain/services/youtube.service.ts
@@ -18,7 +18,7 @@ export interface IYouTubeService {
     url: string,
     quality: string,
     progressCallback: (progress: number) => void,
-    abortSignal?: AbortSignal
+    abortSignal?: unknown
   ): Promise<Buffer>;
 
   /**
